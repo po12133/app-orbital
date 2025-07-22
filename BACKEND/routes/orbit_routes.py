ecae2666d897fe1fa19f5119e33c89b521acbb7f
cvargas3 <cvargas@focoazul.com>
2025-07-22 09:48:52 -0400

    endpoint segunda formula

--- BACKEND/routes/orbit_routes.py
+++ BACKEND/routes/orbit_routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
-from controllers.orbit_controller import primera_formula
+from controllers.orbit_controller import primera_formula, segunda_formula
 
 orbit_bp = Blueprint('orbit', __name__)
 
 # Rutas
 orbit_bp.route('/primera-formula', methods=['POST'])(primera_formula)
+orbit_bp.route('/segunda-formula', methods=['POST'])(segunda_formula)
